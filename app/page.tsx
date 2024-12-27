5901c51ff5c76f1fa46cdc1d48808a9c608e8703
u0empty <yubeen00@naver.com>
2024-12-27 14:11:22 +0900

    CM-107: ✨ 기본 Input 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import Input from "@/components/Input";
+import { useState } from "react";
+
 export default function HomePage() {
-  return <h1>Hello Clmap</h1>;
+  const [isValid, setIsValid] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+
+  const validateInput = (value: string) => {
+    return value.trim() != "";
+  };
+
+  const handleSubmit = () => {
+    const isValidInput = validateInput(inputValue);
+    setIsValid(isValidInput);
+
+    if (!isValidInput) {
+      console.log("입력이 유효하지 않습니다."); // [TODO] 토스트 적용
+    }
+  };
+
+  const handleChange = (value: string) => {
+    setInputValue(value);
+  };
+
+  return (
+    <>
+      <h1>Hello Clmap</h1>
+      <Input
+        size="large"
+        align="left"
+        placeHolder="이메일을 입력해주세요"
+        onChange={handleChange}
+        isValid={isValid}
+      />
+      <button onClick={handleSubmit}>확인</button>
+    </>
+  );
 }
---
+++ components/Input.tsx
@@ -0,0 +1,78 @@
+/** @JSXImportSource @emotion/react **/
+
+import { css } from "@emotion/react";
+import { useState } from "react";
+
+type InputProps = {
+  size: "small" | "medium" | "large";
+  align?: "left" | "center" | "right";
+  placeHolder?: string;
+  defaultValue?: string;
+  onChange: (value: string) => void;
+  isValid?: boolean;
+};
+
+// [TODO] GradeInput.tsx 제작
+const Input: React.FC<InputProps> = ({
+  size,
+  align = "left",
+  placeHolder = "값을 입력해주세요",
+  defaultValue,
+  onChange,
+  isValid = true,
+}) => {
+  const [value, setValue] = useState(defaultValue || "");
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setValue(e.target.value);
+    onChange(e.target.value);
+  };
+
+  const inputStyle = css`
+    outline: none;
+
+    display: flex;
+    gap: 8px;
+
+    align-items: center;
+    justify-content: ${align};
+    padding: 12px 14px;
+    width: ${size === "small" ? "90px" : "100%"};
+    height: 44px;
+
+    border: 1px solid ${isValid ? "#d6d6d6" : "#ff3b30"};
+    border-radius: 10px;
+    font-size: 18px;
+
+    &::placeholder {
+      color: #d6d6d6;
+    }
+  `;
+
+  const textareaStyle = css`
+    ${inputStyle};
+    outline: none;
+    resize: none;
+    height: 102px;
+  `;
+
+  return size === "large" ? (
+    <textarea
+      css={textareaStyle}
+      value={value}
+      onChange={handleChange}
+      placeholder={placeHolder}
+    />
+  ) : (
+    <input
+      css={inputStyle}
+      value={value}
+      onChange={handleChange}
+      placeholder={placeHolder}
+    />
+  );
+};
+
+export default Input;
