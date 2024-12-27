46c39fdb578cbb8e8dd2317f1a9288080d9ba901
u0empty <yubeen00@naver.com>
2024-12-27 12:11:15 +0900

    CM-103: ✨ Header 제작

---
+++ app/visit/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function VisitMainPage() {
+  return (
+    <>
+      <Header title="방문" isBackEnabled={false} />
+      <h1>Hello Visit MainPage</h1>
+    </>
+  );
+}
