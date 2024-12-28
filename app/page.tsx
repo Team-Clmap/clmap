46cf654a8ad1ee8267a6c74e9e9415a796880848
u0empty <yubeen00@naver.com>
2024-12-29 01:18:29 +0900

    CM-108: ✨ 라디오 기본틀 제작

--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,35 @@
 
 "use client";
 
-import Popup from "@/components/Popup";
-import { css } from "@emotion/react";
+import RadioButton from "@/components/RadioButton";
 import { useState } from "react";
 
 export default function HomePage() {
-  const [isPopupOpen, setPopupOpen] = useState(false);
-  const handleButtonClick = () => {
-    setPopupOpen(false);
-  };
+  const [selectedValue, setSelectedValue] = useState("latest");
 
-  const handleLeftButtonClick = () => {
-    setPopupOpen(false);
+  const handleRadioChange = (value: string) => {
+    setSelectedValue(value);
   };
-
-  const buttonStyle = css`
-    width: 100px;
-    height: 40px;
-  `;
 
   return (
     <>
       <h1>Hello Clmap</h1>
-      <button css={buttonStyle} onClick={() => setPopupOpen(true)}>
-        팝업나와
-      </button>
-      <Popup
-        isOpen={isPopupOpen}
-        title="팝업제목"
-        description="팝업입니다 근데 사실은 팝업이고 어떻게보면 팝업이지요 이정도면 됐겠지요"
-        leftButtonName="취소"
-        buttonName="저장"
-        onClickButton={handleButtonClick}
-        onClickLeft={handleLeftButtonClick}
-      >
-        <input />
-      </Popup>
+      <div css={{ display: "flex", gap: "20px" }}>
+        <RadioButton
+          label="최신순"
+          name="filter"
+          value="latest"
+          checked={selectedValue === "latest"}
+          onChange={handleRadioChange}
+        />
+        <RadioButton
+          label="오래된순"
+          name="filter"
+          value="oldest"
+          checked={selectedValue === "oldest"}
+          onChange={handleRadioChange}
+        />
+      </div>
     </>
   );
 }
---
+++ components/RadioButton.tsx
@@ -0,0 +1,75 @@
+/** @JSXImportSource @emotion/react **/
+
+import { css } from "@emotion/react";
+
+type RadioButtonType = {
+  name: string;
+  label: string;
+  value: string;
+  checked?: boolean;
+  onChange?: (value: string) => void;
+};
+
+const RadioButton: React.FC<RadioButtonType> = ({
+  name,
+  label,
+  value,
+  checked,
+  onChange,
+}) => {
+  const radioBoxStyle = css`
+    background-color: aquamarine;
+    align-content: center;
+    width: fit-content;
+  `;
+
+  const radioButtonStyle = css`
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    margin-right: 6px;
+    border: 2px solid #83bbff;
+    border-radius: 50%;
+    position: relative;
+
+    &:checked::after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      background-color: #83bbff;
+    }
+  `;
+
+  const labelStyle = css`
+    background-color: red;
+    font-size: 18px;
+    line-height: 20px;
+    cursor: pointer;
+  `;
+
+  const id = `${name}-${value}`;
+
+  return (
+    <div css={radioBoxStyle}>
+      <input
+        type="radio"
+        css={radioButtonStyle}
+        id={id}
+        name={name}
+        value={value || ""}
+        checked={checked}
+        onChange={() => onChange?.(value)}
+      />
+      <label css={labelStyle} htmlFor={id}>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default RadioButton;
