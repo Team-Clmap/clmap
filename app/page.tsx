4bce4851c635428fe500005ebd5127d781dc984b
u0empty <yubeen00@naver.com>
2024-12-29 18:13:37 +0900

    CM-109: ✨ SearchField 제작 및 Header 태그 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,31 @@
 
 "use client";
 
-import Popup from "@/components/Popup";
-import { css } from "@emotion/react";
 import { useState } from "react";
+import SearchField from "@/components/SearchField";
 
 export default function HomePage() {
-  const [isPopupOpen, setPopupOpen] = useState(false);
-  const handleButtonClick = () => {
-    setPopupOpen(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchChange = (value: string) => {
+    console.log("입력중: ", value);
+    setSearchValue(value);
   };
 
-  const handleLeftButtonClick = () => {
-    setPopupOpen(false);
+  const handleSearchSubmit = (value: string) => {
+    console.log("검색할 암장: ", value);
   };
-
-  const buttonStyle = css`
-    width: 100px;
-    height: 40px;
-  `;
 
   return (
     <>
       <h1>Hello Clmap</h1>
-      <button css={buttonStyle} onClick={() => setPopupOpen(true)}>
-        팝업나와
-      </button>
-      <Popup
-        isOpen={isPopupOpen}
-        title="팝업제목"
-        description="팝업입니다 근데 사실은 팝업이고 어떻게보면 팝업이지요 이정도면 됐겠지요"
-        leftButtonName="취소"
-        buttonName="저장"
-        onClickButton={handleButtonClick}
-        onClickLeft={handleLeftButtonClick}
-      >
-        <input />
-      </Popup>
+      <SearchField
+        size="small"
+        placeholder="암장 이름을 검색해보세요."
+        value={searchValue}
+        onSearch={handleSearchSubmit}
+        onChange={handleSearchChange}
+      />
     </>
   );
 }
