bd8fc7b10d3f54469350f962a17165a3be21ca3e
u0empty <yubeen00@naver.com>
2025-01-06 00:20:58 +0900

    CM-130: 🔨 미니 버튼 추가, 코드 최적화

--- app/page.tsx
+++ app/page.tsx
@@ -2,52 +2,21 @@
 
 "use client";
 
-import Button from "@/components/Button";
+import DatePicker from "@/components/DatePicker";
+import { useState } from "react";
+import SearchField from "@/components/SearchField";
 
 export default function HomePage() {
+  const [date, setDate] = useState("");
+
+  const handleDateChange = (value: string) => {
+    setDate(value);
+  };
+
   return (
-    <div css={{ display: "flex", flexDirection: "column", gap: "8px" }}>
-      <Button type="main" name="버튼하기" onClick={() => console.log("main")} />
-      <Button
-        type="main"
-        name="버튼하기"
-        onClick={() => console.log("main")}
-        isActive={false}
-      />
-      <Button type="sub" name="버튼하기" onClick={() => console.log("sub")} />
-      <Button
-        type="sub"
-        name="버튼하기"
-        onClick={() => console.log("sub")}
-        isActive={false}
-      />
-      <Button
-        type="sub"
-        name="버튼하기"
-        width="half"
-        onClick={() => console.log("sub half")}
-      />
-      <Button
-        type="main"
-        name="버튼하기"
-        width="half"
-        onClick={() => console.log("main half")}
-      />
-      <Button
-        type="google"
-        name="버튼하기"
-        onClick={() => console.log("google")}
-      />
-      <Button
-        type="naver"
-        name="버튼하기"
-        onClick={() => console.log("naver")}
-      />
-      <Button
-        type="kakao"
-        name="버튼하기"
-        onClick={() => console.log("kakao")}
-      />
-    </div>
+    <>
+      <h1>Hello Clmap</h1>
+      <DatePicker size="large" value={date} onChange={handleDateChange} />
+    </>
   );
 }
