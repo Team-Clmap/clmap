6f31eba8d4f29db7d3eb196f88a78b96aa2f9ae5
jinwooseok <jinus7949@naver.com>
2025-01-06 22:03:05 +0900

    CM-181: 🔨 typeorm 설정 - typeorm은 db 매니저의 역할로 전역에서 db를 관리해줌 - Java의 JPA처럼 entity를 만들고 DB와 연동함

--- utils/db.ts
+++ utils/db.ts
@@ -1,38 +1,30 @@
-import { createPool } from "mysql2";
-import type { Pool } from "mysql2";
-//connection pool을 사용하면 항상 새로운 연결을 하는게 아니라 재사용 가능함. 
-const pool: Pool = createPool({
-    user : process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: 3306,
-    database: process.env.DB,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+import { DataSource } from "typeorm";
+
+import 'reflect-metadata';
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  synchronize: false,
+  logging: ['query', 'error'],
+  entities: [],
+  subscribers: [],
+  migrations: [],
 });
 
-pool.getConnection((err, conn) => {
-    if (err) console.log('Error connecting to db...')
-    else console.log('Connected to db...!')
-    conn.release()
-})
+let dataSource: DataSource | undefined;
 
-const executeQuery = (query: string, arrParams: any) => {
-    return new Promise((resolve, reject) => {
-        try {
-            pool.query(query, arrParams, (err, data) => {
-                if (err) {
-                    console.log('Error in executing the query')
-                    reject(err)
-                }
-                //console.log(data)
-                resolve(data)
-            })
-        } catch (err) {
-            reject(err)
-        }
-    })
-}
-
-export default executeQuery
+export const getDataSource = async ()=> {
+  if (!dataSource) {
+    dataSource = AppDataSource;
+    console.log(`create DataSource`);
+    await dataSource.initialize().then(() => {
+      console.log(`connect complete`);
+    });
+  }
+  return dataSource;
+};
