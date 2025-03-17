68277e3dac74f50ac9eb49f4586ba708efbfcd1f
u0empty <yubeen00@naver.com>
2025-03-17 21:56:39 +0900

    CM-174: 🔨 Button icon 분기처리 리팩토링 및 온보딩 화면 중복 렌더링 해결

--- app/onBoarding/page.tsx
+++ app/onBoarding/page.tsx
@@ -6,25 +6,32 @@ import { signIn, useSession } from "next-auth/react";
 import Button from "@/components/Button";
 import { css } from "@emotion/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const OnBoardingPage = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
+  const [clickable, setClickable] = useState(false);
+  
+  useEffect(() => {
+    console.log("useEffect");
+    console.log("status: ", status);
+    console.log("session: ", session);
+  });
 
-  // [TODO] status 가져오는 도중 소셜로그인 버튼 클릭 시 닉네임 화면 반복 이동 이슈 해결
-  // [TODO] 온보딩 후에도 isInited: false로 잡히는 이슈 해결
+  // [TODO] 온보딩 후에도 isInited: false로 잡히는 이슈 해결중(우식)
   useEffect(() => {
     if (status === "loading") return;
-    if (status === "authenticated" && session) {
-      if (session.isNewUser || !session.isInited) {
-        // 로그인O & 온보딩X: 온보딩(닉네임) 화면으로 이동
+    if (status === "authenticated") {
+      if (!session.isNewUser || !session.isInited) {
+        // 로그인O & 온보딩X: 온보딩(닉네임 생성) 화면으로 이동
         router.replace("/onBoarding/nickname");
       } else {
         // 로그인O & 온보딩O: 지도탭으로 이동
         router.replace("/map");
       }
     }
+    setClickable(true);
   }, [session, status]);
 
   return (
@@ -41,22 +48,26 @@ const OnBoardingPage = () => {
           type="kakao"
           buttonName="카카오 로그인"
           onClick={() => signIn("kakao")}
+          isActive={clickable}
         />
         <Button
           type="naver"
           buttonName="네이버 로그인"
           onClick={() => signIn("naver")}
+          isActive={clickable}
         />
         <Button
           type="google"
           buttonName="구글 로그인"
           onClick={() => signIn("google")}
+          isActive={clickable}
         />
         <div css={textWithLineStyle}>또는</div>
         <Button
           type="sub"
           buttonName="비회원으로 시작하기"
           onClick={() => router.push("/map")}
+          isActive={clickable}
         />
       </div>
     </div>
